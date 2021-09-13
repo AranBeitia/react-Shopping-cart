93079de56eea57b0f6772509ce629693ca49f527
AranBeitia <arantza.beitia@gmail.com>
2021-09-13 14:57:59 +0200

    home

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import Main from "./components/Main";
+// import Main from "./components/Main";
+import Home from "./pages/Home";
 
 function App() {
-  return <Main />;
+  return <Home />;
 }
 
 export default App;
