1b291a4b88809cb1fbd109561a6e3f71a6fdbee7
AranBeitia <arantza.beitia@gmail.com>
2021-09-17 14:27:37 +0200

    last changes

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -39,9 +39,10 @@ function ItemCard({
 
   return (
     <article className="item-card col col-12 col-md-6 col-lg-4">
-      <header>
+      <header className="position-relative">
         <img src={img} className="item-card__image" alt={title} />
         <FavoriteIconButton
+          className="item-card__fav-icon"
           handleSetFavorite={onSetFavorite}
           isFavourite={isFavourite}
         />
@@ -51,15 +52,21 @@ function ItemCard({
       <Divider />
       <p>{shortDescription}</p>
       <Divider />
-      <footer className="d-flex">
-        <IconButton aria-label="down vote product" handleClick={onDownVote}>
-          <ThumbDown />
-        </IconButton>
-        <p>{downVotes.currentValue}</p>
-        <IconButton aria-label="up vote product" handleClick={onUpVote}>
-          <ThumbUp />
-        </IconButton>
-        <p>{upVotes.currentValue}</p>
+      <footer className="d-flex justify-content-between">
+        <div className="d-flex justify-content-start">
+          <div className="d-flex align-items-center">
+            <IconButton aria-label="down vote product" handleClick={onDownVote}>
+              <ThumbDown />
+            </IconButton>
+            <p className="mx-1">{downVotes.currentValue}</p>
+          </div>
+          <div className="d-flex align-items-center">
+            <IconButton aria-label="up vote product" handleClick={onUpVote}>
+              <ThumbUp />
+            </IconButton>
+            <p className="mx-1">{upVotes.currentValue}</p>
+          </div>
+        </div>
         <Button name="add to cart" onClick={onAddToCart}>
           Add to cart
         </Button>
